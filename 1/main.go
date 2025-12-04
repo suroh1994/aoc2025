ca7b9483f465f6788532f8fdddf6797557fdb075
Sebastian Jung <jung@denic.de>
2025-12-04 16:39:29 +0100

    Day 1 Part 2

--- 1/main.go
+++ 1/main.go
@@ -9,23 +9,35 @@ func main() {
 	instructions := lib.ReadInputAsLines(1)
 
 	timesLandedOnZero := 0
+	timesPassedOrLandedOnZero := 0
 
 	position := 50
 	for _, instruction := range instructions {
 		// parse instruction
 		amount := lib.MustParseToInt(instruction[1:])
+		fullRotations := amount / 100
+		amount %= 100
+		timesPassedOrLandedOnZero += fullRotations
+
 		if instruction[0] == 'L' {
 			amount *= -1
 		}
 
-		// apply rotation and reset into value range 0-99
-		position = (position + amount) % 100
+		// apply rotation
+		position += amount
+		if (position <= 0 && position != amount) || position >= 100 {
+			timesPassedOrLandedOnZero++
+		}
 
-		// count
+		// reset into value range 0-99
+		position = (position + 100) % 100
+
+		// count zero position
 		if position == 0 {
 			timesLandedOnZero++
 		}
 	}
 
 	fmt.Println(timesLandedOnZero)
+	fmt.Println(timesPassedOrLandedOnZero)
 }
