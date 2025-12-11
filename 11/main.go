3ab74171e18051f69ef8a74fd8232fe54eac0a5a
Sebastian Jung <jung@denic.de>
2025-12-11 18:17:40 +0100

    Day 11 Part 2

--- 11/main.go
+++ 11/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"slices"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -19,26 +20,64 @@ func main() {
 		}
 	}
 
-	startLabel := "you"
-	pathsToEnd := findPathToEnd(connections, startLabel, nil)
+	youLabel := "you"
+	endLabel := "out"
+	pathsToEnd := findPathToTarget(connections, youLabel, endLabel, nil, map[string]int{})
 
 	fmt.Println(pathsToEnd)
+
+	//~~~~~ Part 2 ~~~~~//
+	serverRackLabel := "svr"
+	dacLabel := "dac"
+	fftLabel := "fft"
+	connectionsFromSvrToDac := findPathToTarget(connections, serverRackLabel, dacLabel, nil, map[string]int{})
+	connectionsFromSvrToFft := findPathToTarget(connections, serverRackLabel, fftLabel, nil, map[string]int{})
+	connectionsFromDacToFft := findPathToTarget(connections, dacLabel, fftLabel, nil, map[string]int{})
+	connectionsFromFftToDac := findPathToTarget(connections, fftLabel, dacLabel, nil, map[string]int{})
+	connectionsFromDacToEnd := findPathToTarget(connections, dacLabel, endLabel, nil, map[string]int{})
+	connectionsFromFftToEnd := findPathToTarget(connections, fftLabel, endLabel, nil, map[string]int{})
+
+	connectionsThroughDACAndFFT := connectionsFromSvrToDac*connectionsFromDacToFft*connectionsFromFftToEnd +
+		connectionsFromSvrToFft*connectionsFromFftToDac*connectionsFromDacToEnd
+	fmt.Println(connectionsThroughDACAndFFT)
 }
 
-func findPathToEnd(connections map[string][]string, currentLabel string, labelsVisited []string) int {
+var (
+	knownMutex = sync.Mutex{}
+)
+
+func findPathToTarget(connections map[string][]string, currentLabel, targetLabel string, labelsVisited []string, knownRoutes map[string]int) int {
+	knownMutex.Lock()
+	if routes, exists := knownRoutes[currentLabel]; exists {
+		knownMutex.Unlock()
+		return routes
+	}
+	knownMutex.Unlock()
+
 	if slices.Contains(labelsVisited, currentLabel) {
 		return 0
 	}
 
-	if currentLabel == "out" {
+	if currentLabel == targetLabel {
 		return 1
 	}
 
 	pathsToEnd := 0
+	wg := sync.WaitGroup{}
+	m := sync.Mutex{}
 	for _, connection := range connections[currentLabel] {
-		newLabelsVisited := append(labelsVisited, currentLabel)
-		pathsToEnd += findPathToEnd(connections, connection, newLabelsVisited)
+		wg.Go(func() {
+			newLabelsVisited := append(labelsVisited, currentLabel)
+			pathsFound := findPathToTarget(connections, connection, targetLabel, newLabelsVisited, knownRoutes)
+			m.Lock()
+			pathsToEnd += pathsFound
+			m.Unlock()
+		})
 	}
+	wg.Wait()
 
+	knownMutex.Lock()
+	knownRoutes[currentLabel] = pathsToEnd
+	knownMutex.Unlock()
 	return pathsToEnd
 }
